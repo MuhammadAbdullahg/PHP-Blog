bb414dee7f902e8a1a712d14b2024385fafa7705
MuhammadAbdullahg <jdifn74@gmail.com>
2026-08-03 17:39:50 +0500

    refacter html duplication

--- views/posts/index.view.php
+++ views/posts/index.view.php
@@ -9,24 +9,6 @@
           <img src="images/hero.png" alt="hero" />
         </div>
       </section>
-      <?php if(isset($_SESSION['user_id'])) : ?>
-          <div>
-            <?php foreach($posts as $post) : ?>
-              <div class="h-card" style="display: flex; align-items: center; justify-content: space-between;">
-              <div class="card_content">
-                <h2><?= $post['title'] ?></h2>
-                <p>
-                  <?= $post['content'] ?>
-                </p>
-                <div class="status">
-                  <p><?= $post['created_at'] ?></p>
-                  <a href="/PHP-blog/public/post?id=<?= $post['id'] ?>"><button class="read-btn">Read More</button></a>
-                </div>
-              </div>
-                <img src="<?= $post['image_path'] ?>" alt="laptop guy" />
-              </div>
-            <?php endforeach; ?>
-          </div>
-      <?php endif; ?>
+      <?php require __DIR__ . '/posts.view.php' ?>
     </main>
 <?php require __DIR__ . '/../footer.php' ?>
