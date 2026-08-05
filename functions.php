41c46f94b39fa82239f13e9a85a4b1a4072f38b2
MuhammadAbdullahg <jdifn74@gmail.com>
2026-08-05 20:11:06 +0500

    check

--- functions.php
+++ functions.php
@@ -27,18 +27,18 @@ function allPosts() {
     return $stmt->fetchAll();
 }
 
-function route(string $route, string $controller) {
-    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+// function route(string $route, string $controller) {
+//     $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-    $requestUri = str_replace("/PHP-blog/public", "", $path);
+//     $requestUri = str_replace("/PHP-blog/public", "", $path);
 
-    $uri = rtrim($requestUri, "/");
+//     $uri = rtrim($requestUri, "/");
 
-    if($uri == "") {
-        $uri = "/";
-    }
+//     if($uri == "") {
+//         $uri = "/";
+//     }
     
-    if($uri === $route) {
-        require __DIR__ . $controller;
-    }
-}
+//     if($uri === $route) {
+//         require __DIR__ . $controller;
+//     }
+// }
