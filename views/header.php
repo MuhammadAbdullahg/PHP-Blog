68cbf163a07524be3457364ad02f193457ee347e
MuhammadAbdullahg <jdifn74@gmail.com>
2026-08-12 15:59:23 +0500

    structure card layout

--- views/header.php
+++ views/header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Home Page | Blog Website</title>
-    <link rel="stylesheet" href="style.css" />
+    <link rel="stylesheet" href="../public/style.css" />
     <script
       src="https://kit.fontawesome.com/7a4b62b0a4.js"
       crossorigin="anonymous"
@@ -15,7 +15,7 @@
   <body>
     <header>
       <nav>
-        <h1>Great Zone</h1>
+        <h1><img src="../public/images/logo.png" style="width: 50px;" alt=""></h1>
         <ul>
           <a href="<?= $commonPath ?>">
             <li>Home</li>
--- views/posts/postsData.view.php
+++ views/posts/postsData.view.php
@@ -1,19 +1,23 @@
 <?php require __DIR__ . '/../../config/config.php' ?>
     <?php if(!empty($posts)) : ?>
-          <div>
+          <div style="width: 100%; display: flex; flex-wrap: wrap; align-items:center; justify-content: space-between; ">
             <?php foreach($posts as $post) : ?>
-              <div class="h-card" style="display: flex; align-items: center; justify-content: space-between;">
-              <div class="card_content">
+              <?php 
+              $description = $post['content'];
+              $shortText = substr($description, 0, 30) . '...';
+              ?>
+              <div class="h-card" style="flex-direction: column; width: 300px; height: 300px;">
+              <div class="card_content" style="height: 200px;">
+                <img src="<?= $post['image_path'] ?>" style="width: 200px; height: 150px;" alt="laptop guy" />
+              </div>
                 <h2><?= $post['title'] ?></h2>
                 <p>
-                  <?= $post['content'] ?>
+                  <?= $shortText ?>
                 </p>
                 <div class="status">
-                  <p><?= $post['created_at'] ?></p>
-                  <a href="<?= $commonPath ?>post?id=<?= $post['id'] ?>"><button class="read-btn">Read More</button></a>
+                  <p style="padding: 1rem;"><?= $post['created_at'] ?></p>
+                  <a href="<?= $commonPath ?>post?id=<?= $post['id'] ?>" style="padding: 1rem;"><button class="read-btn">Read More</button></a>
                 </div>
-              </div>
-                <img src="<?= $post['image_path'] ?>" alt="laptop guy" />
               </div>
             <?php endforeach; ?>
           </div>
